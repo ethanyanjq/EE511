f7018030df90ee30fd56237d5383cc6792ccf81d
Jingquan Yan <ethanyanjq@gmail.com>
2019-09-08 21:24:41 -0700

    HW2 Problem1 finished

---
+++ HW2/Question1.py
@@ -0,0 +1,34 @@
+import numpy as np
+from numpy import random as nr
+from matplotlib import pyplot as plt
+
+def bs_resample(data, alpha):
+    Nk = 100 #resample times
+    length = len(data)
+    bs_mean = []
+    bs_var = []
+    for i in range(Nk):
+        new_seq = []
+        for j in range(length):
+            num = nr.randint(0, length)
+            new_seq.append(data[num])
+        bs_mean.append(np.mean(new_seq))
+        bs_var.append(np.var(new_seq))
+    bs_mean = np.sort(bs_mean)
+    bs_var = np.sort(bs_var)
+    return (bs_mean[int((alpha/2)*Nk)], bs_mean[int((1-alpha/2)*Nk)], bs_var[int((alpha/2)*Nk)], bs_var[int((1-alpha/2)*Nk)])
+
+N = 1000
+uni_seq = nr.rand(N)*5 - 3
+average = float(sum(uni_seq))/len(uni_seq)
+print("The sample mean is:" + str(average))
+sum = 0
+for data in uni_seq:
+    sum += (data - average) ** 2
+stdvar = np.sqrt(sum/(N-1))
+print("The sample variance is "+ str(stdvar))
+plt.hist(uni_seq)
+plt.show()
+result = bs_resample(uni_seq, 0.05)
+print(result)
+
