ffed684b39ef5810e76f5c08ff7fd09ad4c15e3e
ethanyanjq <ethanyan@163.com>
2019-09-22 17:03:43 -0700

    HW4 problem 3 finished

--- HW4/Problem3.py
+++ HW4/Problem3.py
@@ -1,42 +1,49 @@
 import numpy as np
 import math
+import xlrd
 import matplotlib.pyplot as plt
-import scipy.stats as stats
 
-n = 100
-def func(n):
-    X = [0]*n
-    for i in range(n):
-        temp = 0
-        for j in range(4):
-            temp += (np.random.randn())**2
-        X[i] = temp
-    return sorted(X)
+def bs_resample(data):
+    Nk = 1000 #resample times
+    length = len(data)
+    bs_mean = []
+    bs_var = []
+    alpha = 0.95
+    for i in range(Nk):
+        new_seq = []
+        for j in range(length):
+            num = np.random.randint(0, length)
+            new_seq.append(data[num])
+        bs_mean.append(np.mean(new_seq))
+    bs_mean = np.sort(bs_mean)
+    return (bs_mean[int((alpha/2)*Nk)], bs_mean[int((1-alpha/2)*Nk)])
 
-X = func(n)
-X.insert(0, 0)
-X.append(20)
-Y = np.arange(0, 1, 1/n)
-Y = list(Y)
-Y.append(1)
-Y.insert(0, 0)
+workbook = xlrd.open_workbook(r'data.xlsx')
+sheet = workbook.sheet_by_index(0)
+duration = sheet.col_values(1)
+waiting = sheet.col_values(2)
 
-temp = 0
-percentile = 0
-s = 0
-for i in range(len(X)):
-    if (abs(stats.chi2.cdf(X[i], df=4) - Y[i]) > temp) :
-        temp = abs(stats.chi2.cdf(X[i], df=4) - Y[i])
-    if (Y[i]>0.25 and s == 0):
-        s = 1
-        percentile = X[i]
-        
-print(temp)
-print(percentile)
-print (stats.chi2.ppf(0.25, 4))
-
-plt.step(X, Y)
-plt.plot(np.linspace(0,20,100),stats.chi2.cdf(np.linspace(0,20,100),df=4))
+duration_15 = duration[:15]
+waiting_15 = waiting[:15]
+waiting_list = list(set(waiting))
+plt.hist(waiting, bins = len(waiting_list))
 plt.show()
 
+std_dev = np.std(waiting_15, ddof = 1)
+mean = np.mean(waiting_15)
+t = 2.1448
+upper_bound = mean + (std_dev/math.sqrt(15))*t
+lower_bound = mean - (std_dev/math.sqrt(15))*t
+print(upper_bound, lower_bound)
+bs_result = bs_resample(waiting_15)
+print(bs_result[0], bs_result[1])
+
+std_dev = np.std(waiting, ddof = 1)
+mean = np.mean(waiting)
+t = 1.960
+upper_bound = mean + (std_dev/math.sqrt(15))*t
+lower_bound = mean - (std_dev/math.sqrt(15))*t
+print(upper_bound, lower_bound)
+bs_result = bs_resample(waiting)
+print(bs_result[0], bs_result[1])
 
