913577aea8f3de4166542c2a2c80d1856a36af95
ethanyanjq <ethanyan@163.com>
2019-10-26 10:07:00 -0700

    Prob 1 and 2 solved

--- HW6/Problem1.py
+++ HW6/Problem1.py
@@ -22,7 +22,7 @@ def Generate (n, n1, n2, d1, d2):
     return (X_seq, Y_seq, Sum)
     
 result = Generate(10000, 1, 2, 4, 9)
-plt.hist(result[2], normed = 1, bins = 300, alpha = 0.9, label = 'N(1,4)+N(2,9)')
+plt.hist(result[2], density = 1, bins = 300, alpha = 0.9, label = 'N(1,4)+N(2,9)')
 
 x = np.arange(-40,40,0.1)
 y = theoretical(x, 3, 13**0.5)
@@ -30,7 +30,8 @@ plt.plot(x,y, color='r',linewidth = 2, label = 'Theoretical pdf')
 plt.legend()
 plt.show()
 
-print("The covariance of X and Y is: {}".format(np.cov(result[0], result[1])))
+print("The covariance of X and Y is: \n {}".format(np.cov(result[0], result[1])))
+print("The mean of A is : {} and the variance is :{}".format(np.mean(result[2]), np.var(result[2])))
        
         
 
---
+++ HW6/Problem2.py
@@ -0,0 +1,47 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+ 
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return (n * factorial(n - 1))  
+
+def gamma_func(theta):
+    n = int(math.floor(theta))
+    return (factorial(2 * n) * (math.pi ** 0.5)) / ( (4 ** n) * factorial(n))
+    
+def gamma_dist(a, b, x):
+    gamma = gamma_func(a)
+    return (x**(a-1)) * ((1/b)**a) * (math.e**(-x/b)) / gamma
+    
+def gamma_list(a, b):
+    seq = []
+    x = np.linspace(0, 25, 200)
+    gamma = gamma_func(a)
+    for i in range(len(x)):
+        seq.append((x[i]**(a-1)) * ((1/b)**a) * (math.e**(-x[i]/b)) / gamma)
+    return seq
+
+def exp_bound(lambd, x):
+    return lambd*np.exp(-lambd*(x-4))
+
+N = 10000
+y = []
+accept = 0
+for i in range(N):
+    rand_1 = 25 * np.random.rand()
+    rand_2 = exp_bound(0.3, rand_1) * np.random.rand()
+    if (rand_2 <= gamma_dist(5.5, 1, rand_1)): 
+        y.append(rand_1)
+        accept += 1
+print("The efficiency of accept and reject is {}".format(accept/N))   
+plt.hist(y, density = 1, bins = 100)
+
+a = gamma_list(5.5,1)
+x = np.linspace(0, 25, 200)
+plt.plot(x, a)
+plt.show()
+
