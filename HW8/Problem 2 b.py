54b00d5b26c797bcb809b7041210ff23a97a6795
ethanyanjq <ethanyan@163.com>
2019-11-16 22:27:16 -0800

    HW8 Problem 2 solved

---
+++ HW8/Problem 2 b.py
@@ -0,0 +1,19 @@
+import numpy as np
+import math
+
+N = 1000
+x = [[0.1], [0.1], [0.1]]
+for i in range(N):
+    m = math.floor(3 * np.random.rand())
+    sum = 0
+    e = [0,1,2]
+    e.remove(m)
+    for i in e:
+        sum += (i+1)*x[i][-1]
+    rest = 1 - sum
+    while 1:
+        temp = np.random.exponential(1)
+        if temp * (m+1) < rest: break
+    x[m].append(temp)
+
+print(np.mean(x[0]+2*np.mean(x[1])+3*np.mean(x[2])))
---
+++ HW8/test.py
@@ -0,0 +1,4 @@
+import numpy as np
+a = [1,2,3]
+a.remove(2)
+print(a)
