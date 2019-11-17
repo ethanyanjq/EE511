588d67886455493b07cf974aff029f0e2199725a
jyan97 <ethanyan@163.com>
2019-11-17 14:41:35 -0800

    HW8 Problem 2

--- HW8/Problem 2 a.py
+++ HW8/Problem 2 a.py
@@ -6,14 +6,14 @@ x = [[1], [2], [3]]
 for i in range(N):
     m = math.floor(3 * np.random.rand())
     sum = 0
-    e = [0,1,2]
+    e = [0, 1, 2]
     e.remove(m)
-    for i in e:
-        sum += (i+1)*x[i][-1]
+    for j in e:
+        sum += (j + 1) * x[j][-1]
     rest = 15 - sum
     while 1:
         temp = np.random.exponential(1)
-        if temp * (m+1) > rest: break
+        if temp * (m + 1) > rest: break
     x[m].append(temp)
 
-print(np.mean(x[0]+2*np.mean(x[1])+3*np.mean(x[2])))
+print(np.mean(x[0] + 2 * np.mean(x[1]) + 3 * np.mean(x[2])))
