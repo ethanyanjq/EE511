bdde0406e0cbf33c74dc24c1552ab3727817e0a7
jyan97 <ethanyan@163.com>
2019-11-28 00:40:24 -0800

    Problem 3

---
+++ HW8/Problem 3.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import math
+import random
+import numpy as np
+from diagram import print_diagram
+
+
+def judge(dE, t):
+    if dE < 0:
+        out_put = 1
+    else:
+        d = math.exp(-dE / t)
+        if d > np.random.rand():
+            out_put = 1
+        else:
+            out_put = 0
+    return out_put
+
+
+def func_calculation(x, y):
+    return 837.9658 - x * math.sin(math.sqrt(abs(x))) - y * math.sin(math.sqrt(abs(y)))
+
+
+lower_bound, upper_bound = -500, 500
+t, counter = 0, 0
+temp = 1e4
+min_temp = 1
+# alpha = 0.99
+
+x_old = (upper_bound - lower_bound)*np.random.rand() + lower_bound
+y_old = (upper_bound - lower_bound)*np.random.rand() + lower_bound
+x_new, y_new = x_old, y_old
+s_old = func_calculation(x_old,y_old)
+s_new = s_old
+
+while(temp > min_temp):
+    delta = np.random.normal(0,1) * 500
+    x_new = x_old + delta
+    if x_new < lower_bound or x_new > upper_bound:
+        x_new = x_new - delta
+    delta = np.random.normal(0,1) * 500
+    y_new = y_old + delta
+    if y_new < lower_bound or y_new > upper_bound:
+        y_new = y_new - delta
+    s_new = func_calculation(x_new, y_new)
+
+    dE = s_old - s_new
+    j = judge(dE, temp)
+    if j:
+        s_old = s_new
+        x_old = x_new
+        y_old = y_new
+    if dE<0: temp = 1e4/(1+t)
+    else: counter = counter + 1
+    t += 1
+    plt.scatter([x_old],[y_old],marker='x',c='#DC143C')
+    if counter>1e4:
+        break
+
+plt.show()
+print(x_old,y_old,s_old)
