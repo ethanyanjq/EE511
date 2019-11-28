f09c12dd23afc59128f4ae1a48cbefa34d839aaa
ethanyanjq <ethanyan@163.com>
2019-11-28 01:20:01 -0800

    HW8 P3

--- HW8/Problem 3.py
+++ HW8/Problem 3.py
@@ -5,11 +5,11 @@ import numpy as np
 from diagram import print_diagram
 
 
-def judge(dE, t):
-    if dE < 0:
+def judge(d, t):
+    if d < 0:
         out_put = 1
     else:
-        d = math.exp(-dE / t)
+        d = math.exp(-d/(15*t))
         if d > np.random.rand():
             out_put = 1
         else:
@@ -25,20 +25,19 @@ lower_bound, upper_bound = -500, 500
 t, counter = 0, 0
 temp = 1e4
 min_temp = 1
-# alpha = 0.99
 
-x_old = (upper_bound - lower_bound)*np.random.rand() + lower_bound
-y_old = (upper_bound - lower_bound)*np.random.rand() + lower_bound
+x_old = (upper_bound - lower_bound) * np.random.rand() + lower_bound
+y_old = (upper_bound - lower_bound) * np.random.rand() + lower_bound
 x_new, y_new = x_old, y_old
-s_old = func_calculation(x_old,y_old)
+s_old = func_calculation(x_old, y_old)
 s_new = s_old
 
-while(temp > min_temp):
-    delta = np.random.normal(0,1) * 500
+while temp > min_temp:
+    delta = np.random.normal(0, 150)
     x_new = x_old + delta
     if x_new < lower_bound or x_new > upper_bound:
         x_new = x_new - delta
-    delta = np.random.normal(0,1) * 500
+    delta = np.random.normal(0, 150)
     y_new = y_old + delta
     if y_new < lower_bound or y_new > upper_bound:
         y_new = y_new - delta
@@ -50,12 +49,14 @@ while(temp > min_temp):
         s_old = s_new
         x_old = x_new
         y_old = y_new
-    if dE<0: temp = 1e4/(1+t)
-    else: counter = counter + 1
+    if dE < 0:
+        temp = 1e3 / (1 + t)
+    else:
+        counter += 1
     t += 1
-    plt.scatter([x_old],[y_old],marker='x',c='#DC143C')
-    if counter>1e4:
+    plt.scatter([x_old], [y_old], marker='x', c='#DC143C')
+    if counter > 1e3:
         break
 
 plt.show()
-print(x_old,y_old,s_old)
+print(x_old, y_old, s_old)
