e416b5c0c96609755998c46d7180894ca73d8dc0
jyan97 <ethanyan@163.com>
2019-11-30 02:38:15 -0800

    HW8 finishedgit add *git add *git add *

--- HW8/Problem 4.py
+++ HW8/Problem 4.py
@@ -1,13 +1,14 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import time
 
 
 def judge(d, t):
     if d < 0:
         out_put = 1
     else:
-        d = math.exp(-d / (5 * t))
+        d = math.exp(-d / (10*t)) #parameter 1
         if d > np.random.rand():
             out_put = 1
         else:
@@ -17,14 +18,54 @@ def judge(d, t):
 
 def dist(states):
     dist_sum = 0
-    for i in range(len(states) - 1):
-        dist_sum += (states[i + 1][0] - states[i][0]) ** 2 + (states[i + 1][1] - states[i][1]) ** 2
+    for i in range(states.shape[1] - 1):
+        dist_sum += (states[0, i + 1] - states[0, i]) ** 2 + (states[1, i + 1] - states[1, i]) ** 2
     return dist_sum
 
-def solve(city):
 
+def new_solve(states):
+    states = states
+    cities2 = states.copy()
+    while 1:
+        cit1 = math.ceil(np.random.rand() * (states.shape[1] - 1))
+        cit2 = math.ceil(np.random.rand() * (states.shape[1] - 1))
+        if abs(np.random.normal(0, 1)) > 0.3: break
+        temp = cities2[:, cit1].copy()
+        cities2[:, cit1] = cities2[:, cit2].copy()
+        cities2[:, cit2] = temp.copy()
+    return cities2
 
 
 states = np.loadtxt('states.txt')
-plt.plot(states[0], states[1])
-plt.show()
+np.random.shuffle(states)
+states = states.T
+#plt.plot(states[0], states[1])
+#plt.show()
+
+tmp, min_temp = 1e5, 1
+s_old = dist(states)
+cit_new = states
+s_new = s_old
+counter, t = 0, 0
+
+plt.ion()
+while tmp > min_temp:
+    plt.clf()
+    cit_new = new_solve(states)
+    s_new = dist(cit_new)
+    dE = (s_new - s_old) * 1  #parameter 2
+    j = judge(dE, tmp)
+    if j:
+        states = cit_new
+        s_old = s_new
+    if dE < 0:
+        counter = 0
+        tmp = 1e5/(1+t)
+        plt.plot(states[0], states[1])
+        plt.pause(0.005)
+    else:
+        counter += 1
+    t += 1
+
+plt.ioff()
+print(states)
--- HW8/test.py
+++ HW8/test.py
@@ -1,9 +1,6 @@
 import numpy as np
 
-f = np.loadtxt('states.txt')
-sum = 0
-for i in range(len(f) - 1):
-    sum += (f[i+1][0] - f[i][0]) ** 2 + (f[i+1][1] - f[i][1]) ** 2
-print(sum)
+f = np.loadtxt('states1.txt')
+print(f[2])
 
 
