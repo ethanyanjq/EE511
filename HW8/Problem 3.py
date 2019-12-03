81f0d0bd27359949a8bfd55f92ab4ebe09acfabb
jyan97 <ethanyan@163.com>
2019-12-03 10:30:56 -0800

    HW8

--- HW8/Problem 3.py
+++ HW8/Problem 3.py
@@ -1,21 +1,17 @@
 import matplotlib.pyplot as plt
 import math
-import random
 import numpy as np
-from diagram import print_diagram
-
 
 def judge(d, t):
     if d < 0:
         out_put = 1
     else:
-        d = math.exp(-d/(5*t))
+        d = math.exp(-d/(3*t))
         if d > np.random.rand():
             out_put = 1
         else:
             out_put = 0
     return out_put
-
 
 def func_calculation(x, y):
     return 837.9658 - x * math.sin(math.sqrt(abs(x))) - y * math.sin(math.sqrt(abs(y)))
@@ -24,7 +20,7 @@ def func_calculation(x, y):
 lower_bound, upper_bound = -500, 500
 t, counter = 0, 0
 temp = 1e4
-min_temp = 1
+min_temp = 1e-3
 
 x_old = (upper_bound - lower_bound) * np.random.rand() + lower_bound
 y_old = (upper_bound - lower_bound) * np.random.rand() + lower_bound
@@ -32,6 +28,15 @@ x_new, y_new = x_old, y_old
 s_old = func_calculation(x_old, y_old)
 s_new = s_old
 
+plt.ion()
+N_r = 500
+x = np.linspace(-N_r,N_r,100)
+y = np.linspace(-N_r,N_r,100)
+X, Y = np.meshgrid(x, y)
+Z = 837.9658 - X * np.sin(np.sqrt(abs(X))) - Y * np.sin(np.sqrt(abs(Y)))
+
+plt.figure(num=None,dpi=100)
+plt.contourf(X,Y,Z)
 while temp > min_temp:
     delta = np.random.normal(0, 200)
     x_new = x_old + delta
@@ -46,17 +51,17 @@ while temp > min_temp:
     dE = s_old - s_new
     j = judge(dE, temp)
     if j:
-        s_old = s_new
-        x_old = x_new
-        y_old = y_new
+        s_old, x_old, y_old = s_new, x_new, y_new
     if dE < 0:
-        temp = 5e3 / (1 + t)
+        temp = 1e4 / (1 + t)
     else:
         counter += 1
     t += 1
     plt.scatter([x_old], [y_old], marker='x', c='#DC143C')
+    plt.pause(0.001)
     # if counter > 1e3:
     #     break
 
+plt.ioff()
 plt.show()
 print(x_old, y_old, s_old)
